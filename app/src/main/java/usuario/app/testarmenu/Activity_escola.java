26e90f035a8db29234e4803949d75a14cdc30aed
MarcoPSM <marcopaulomartins@hotmail.com>
2016-06-29 15:27:37 +0100

    app pronta

---
+++ app/src/main/java/usuario/app/testarmenu/Activity_escola.java
@@ -0,0 +1,13 @@
+package usuario.app.testarmenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_escola extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_escola);
+    }
+}
