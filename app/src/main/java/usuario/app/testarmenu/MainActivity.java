6a53c47a9d6373264b43f075aa4177a178513f88
MarcoPSM <marcopaulomartins@hotmail.com>
2016-06-29 15:27:05 +0100

    app pronta

--- app/src/main/java/usuario/app/testarmenu/MainActivity.java
+++ app/src/main/java/usuario/app/testarmenu/MainActivity.java
@@ -1,6 +1,8 @@
 package usuario.app.testarmenu;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -8,6 +10,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -16,14 +19,15 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.GroundOverlayOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.android.gms.maps.model.TileProvider;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
 
     private GoogleMap mMap;
+    SQLiteDatabase mydatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +36,29 @@ public class MainActivity extends AppCompatActivity
 
         setContentView(R.layout.activity_main);
 
+
+        mydatabase = openOrCreateDatabase("niugisviewer",MODE_PRIVATE,null);
+
+        mydatabase.execSQL("CREATE TABLE IF NOT EXISTS layers(name VARCHAR, title VARCHAR, active BOOLEAN);");
+        mydatabase.execSQL("DELETE FROM Layers");
+        mydatabase.execSQL("INSERT INTO layers(name, title, active) VALUES('0a4d9a280794d2f152feebbf7423340b', 'freguesias', 1);");
+        mydatabase.execSQL("INSERT INTO layers(name, title, active) VALUES('28ef66cda0ee9a2eb81df5a79e898517', 'edificios', 1);");
+        mydatabase.execSQL("INSERT INTO layers(name, title, active) VALUES('ab2cc899ed871afd2152e997c3934eac', 'vias', 1);");
+        mydatabase.execSQL("INSERT INTO layers(name, title, active) VALUES('6406445d84559cbfe7d072f76438ff1d', 'ferrovias', 1);");
+        mydatabase.execSQL("INSERT INTO layers(name, title, active) VALUES('b55d05c572d87837cb2c498f931f9d27', 'bancos', 1);");
+        mydatabase.execSQL("INSERT INTO layers(name, title, active) VALUES('6168ea2df006cf2d0e104e7be88dff1b', 'bombas_gasolina', 1);");
+        mydatabase.execSQL("INSERT INTO layers(name, title, active) VALUES('2d88045660bbf8fe2ce8a09a4af033ed', 'bombeiros', 1);");
+        mydatabase.execSQL("INSERT INTO layers(name, title, active) VALUES('482bff4aebbd878a0660dd36d0505cc4', 'cafes', 1);");
+        mydatabase.execSQL("INSERT INTO layers(name, title, active) VALUES('3541759ddf6a06a78ba914b2f56f9b44', 'conservatoria_registo_predial', 1);");
+        mydatabase.execSQL("INSERT INTO layers(name, title, active) VALUES('37b9bad64eb1858d5b698095b7bc129c', 'correio', 1);");
+        mydatabase.execSQL("INSERT INTO layers(name, title, active) VALUES('adfdd03c65e9dd24a7cddcab99c35162', 'escola', 1);");
+        mydatabase.execSQL("INSERT INTO layers(name, title, active) VALUES('929aed75a215d6aeddd5e73c2f8ce206', 'estaçao_comboio', 1);");
+        mydatabase.execSQL("INSERT INTO layers(name, title, active) VALUES('7799240f8dbd39c1f6fe306bcfa0f145', 'farmacias', 1);");
+        mydatabase.execSQL("INSERT INTO layers(name, title, active) VALUES('610f3e271d9a29235ab5abc45d6e6e38', 'policia', 1);");
+        mydatabase.execSQL("INSERT INTO layers(name, title, active) VALUES('73b4b7160e10fe06f4f2aa2d0c832163', 'restaurante', 1);");
+        mydatabase.execSQL("INSERT INTO layers(name, title, active) VALUES('aa0d5ea150c6698d7a9dd3eb20fc7703', 'supermercados', 1);");
+
+        setUpMapIfNeeded();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -50,6 +77,7 @@ public class MainActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
         mapFragment.getMapAsync(this);
+
     }
 
     @Override
@@ -69,6 +97,7 @@ public class MainActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.layers_menu, menu);
         return true;
     }
 
@@ -77,6 +106,7 @@ public class MainActivity extends AppCompatActivity
         Intent about = new Intent(this, Activity_about.class);
         Intent cma = new Intent(this, Activity_cma.class);
         Intent ng = new Intent(this, Activity_ng.class);
+        Intent es = new Intent(this, Activity_escola.class);
 
         int id = item.getItemId();
 
@@ -84,14 +114,208 @@ public class MainActivity extends AppCompatActivity
             startActivity(about);
             return true;
         }
+
         if (id == R.id.action_contacto) {
             startActivity(cma);
             return true;
         }
+
         if (id == R.id.action_contacto2) {
             startActivity(ng);
             return true;
         }
+
+        if (id == R.id.action_contacto3) {
+            startActivity(es);
+            return true;
+        }
+        if (id == R.id.layersmenu) {
+            return true;
+        }
+
+
+        if(id == R.id.layer1){
+            Toast.makeText(MainActivity.this, "Freguesia", Toast.LENGTH_LONG).show();
+                if (item.isChecked()) {
+                    item.setChecked(false);
+                    this.mydatabase.execSQL("UPDATE layers SET active=0 WHERE title='freguesias';");
+                } else {
+                    item.setChecked(true);
+                    this.mydatabase.execSQL("UPDATE layers SET active=1 WHERE title='freguesias';");
+                }
+
+
+        }
+
+        if(id == R.id.layer2){
+            Toast.makeText(MainActivity.this, "Edificios", Toast.LENGTH_LONG).show();
+            if (item.isChecked()) {
+                item.setChecked(false);
+                this.mydatabase.execSQL("UPDATE layers SET active=0 WHERE title='edificios';");
+            } else {
+                item.setChecked(true);
+                this.mydatabase.execSQL("UPDATE layers SET active=1 WHERE title='edificios';");
+            }
+        }
+
+        if(id == R.id.layer3){
+            Toast.makeText(MainActivity.this, "Vias", Toast.LENGTH_LONG).show();
+            if (item.isChecked()) {
+                item.setChecked(false);
+                this.mydatabase.execSQL("UPDATE layers SET active=0 WHERE title='vias';");
+            } else {
+                item.setChecked(true);
+                this.mydatabase.execSQL("UPDATE layers SET active=1 WHERE title='vias';");
+            }
+        }
+
+        if(id == R.id.layer4){
+            Toast.makeText(MainActivity.this, "Ferrovias", Toast.LENGTH_LONG).show();
+            if (item.isChecked()) {
+                item.setChecked(false);
+                this.mydatabase.execSQL("UPDATE layers SET active=0 WHERE title='ferrovias';");
+            } else {
+                item.setChecked(true);
+                this.mydatabase.execSQL("UPDATE layers SET active=1 WHERE title='ferrovias';");
+            }
+        }
+
+        if(id == R.id.layer5){
+            Toast.makeText(MainActivity.this, "Bancos", Toast.LENGTH_LONG).show();
+            if (item.isChecked()) {
+                item.setChecked(false);
+                this.mydatabase.execSQL("UPDATE layers SET active=0 WHERE title='bancos';");
+            } else {
+                item.setChecked(true);
+                this.mydatabase.execSQL("UPDATE layers SET active=1 WHERE title='bancos';");
+            }
+        }
+
+        if(id == R.id.layer6){
+            Toast.makeText(MainActivity.this, "Bombas de Gasolina", Toast.LENGTH_LONG).show();
+            if (item.isChecked()) {
+                item.setChecked(false);
+                this.mydatabase.execSQL("UPDATE layers SET active=0 WHERE title='bombas_gasolina';");
+            } else {
+                item.setChecked(true);
+                this.mydatabase.execSQL("UPDATE layers SET active=1 WHERE title='bombas_gasolina';");
+            }
+        }
+
+        if(id == R.id.layer7){
+            Toast.makeText(MainActivity.this, "Bombeiros", Toast.LENGTH_LONG).show();
+            if (item.isChecked()) {
+                item.setChecked(false);
+                this.mydatabase.execSQL("UPDATE layers SET active=0 WHERE title='bombeiros';");
+            } else {
+                item.setChecked(true);
+                this.mydatabase.execSQL("UPDATE layers SET active=1 WHERE title='bombeiros';");
+            }
+        }
+
+
+        if(id == R.id.layer8){
+            Toast.makeText(MainActivity.this, "Cafés", Toast.LENGTH_LONG).show();
+            if (item.isChecked()) {
+                item.setChecked(false);
+                this.mydatabase.execSQL("UPDATE layers SET active=0 WHERE title='cafes';");
+            } else {
+                item.setChecked(true);
+                this.mydatabase.execSQL("UPDATE layers SET active=1 WHERE title='cafes';");
+            }
+        }
+
+        if(id == R.id.layer10){
+            Toast.makeText(MainActivity.this, "Conservatório do Registo Predial", Toast.LENGTH_LONG).show();
+            if (item.isChecked()) {
+                item.setChecked(false);
+                this.mydatabase.execSQL("UPDATE layers SET active=0 WHERE title='conservatoria_registo_predial';");
+            } else {
+                item.setChecked(true);
+                this.mydatabase.execSQL("UPDATE layers SET active=1 WHERE title='conservatoria_registo_predial';");
+            }
+        }
+
+        if(id == R.id.layer11){
+            Toast.makeText(MainActivity.this, "Correios", Toast.LENGTH_LONG).show();
+            if (item.isChecked()) {
+                item.setChecked(false);
+                this.mydatabase.execSQL("UPDATE layers SET active=0 WHERE title='correio';");
+            } else {
+                item.setChecked(true);
+                this.mydatabase.execSQL("UPDATE layers SET active=1 WHERE title='correio';");
+            }
+        }
+
+        if(id == R.id.layer12){
+            Toast.makeText(MainActivity.this, "Escolas", Toast.LENGTH_LONG).show();
+            if (item.isChecked()) {
+                item.setChecked(false);
+                this.mydatabase.execSQL("UPDATE layers SET active=0 WHERE title='escola';");
+            } else {
+                item.setChecked(true);
+                this.mydatabase.execSQL("UPDATE layers SET active=1 WHERE title='escola';");
+            }
+        }
+
+        if(id == R.id.layer9){
+            Toast.makeText(MainActivity.this, "Estação de Comboios", Toast.LENGTH_LONG).show();
+            if (item.isChecked()) {
+                item.setChecked(false);
+                this.mydatabase.execSQL("UPDATE layers SET active=0 WHERE title='estaçao_comboio';");
+            } else {
+                item.setChecked(true);
+                this.mydatabase.execSQL("UPDATE layers SET active=1 WHERE title='estaçao_comboio';");
+            }
+        }
+
+        if(id == R.id.layer13){
+            Toast.makeText(MainActivity.this, "Farmácias", Toast.LENGTH_LONG).show();
+            if (item.isChecked()) {
+                item.setChecked(false);
+                this.mydatabase.execSQL("UPDATE layers SET active=0 WHERE title='farmacias';");
+            } else {
+                item.setChecked(true);
+                this.mydatabase.execSQL("UPDATE layers SET active=1 WHERE title='farmacias';");
+            }
+        }
+
+        if(id == R.id.layer14){
+            Toast.makeText(MainActivity.this, "Polícia", Toast.LENGTH_LONG).show();
+            if (item.isChecked()) {
+                item.setChecked(false);
+                this.mydatabase.execSQL("UPDATE layers SET active=0 WHERE title='policia';");
+            } else {
+                item.setChecked(true);
+                this.mydatabase.execSQL("UPDATE layers SET active=1 WHERE title='policia';");
+            }
+        }
+
+        if(id == R.id.layer16){
+            Toast.makeText(MainActivity.this, "Supermercados", Toast.LENGTH_LONG).show();
+            if (item.isChecked()) {
+                item.setChecked(false);
+                this.mydatabase.execSQL("UPDATE layers SET active=0 WHERE title='supermercados';");
+            } else {
+                item.setChecked(true);
+                this.mydatabase.execSQL("UPDATE layers SET active=1 WHERE title='supermercados';");
+            }
+        }
+
+        if(id == R.id.layer15){
+            Toast.makeText(MainActivity.this, "Restaurantes", Toast.LENGTH_LONG).show();
+            if (item.isChecked()) {
+                item.setChecked(false);
+                this.mydatabase.execSQL("UPDATE layers SET active=0 WHERE title='restaurante';");
+            } else {
+                item.setChecked(true);
+                this.mydatabase.execSQL("UPDATE layers SET active=1 WHERE title='restaurante';");
+            }
+        }
+
+
+        mMap.clear();
+        setUpMap();
         return super.onOptionsItemSelected(item);
     }
 
@@ -105,18 +329,11 @@ public class MainActivity extends AppCompatActivity
             if(mMap.getMapType() == GoogleMap.MAP_TYPE_NORMAL) {
                 mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
             } else mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-        }   else if (id == R.id.nav_gallery) {
-            Toast.makeText(MainActivity.this, "Galeria", Toast.LENGTH_LONG).show();
-        } else if (id == R.id.nav_slideshow) {
-            Toast.makeText(MainActivity.this, "Slideshow", Toast.LENGTH_LONG).show();
-        } else if (id == R.id.nav_manage) {
-            Toast.makeText(MainActivity.this, "Não sei o que estou à fazer.", Toast.LENGTH_LONG).show();
         } else if (id == R.id.nav_share) {
-            Toast.makeText(MainActivity.this, "Partilhar", Toast.LENGTH_LONG).show();
+            Toast.makeText(MainActivity.this, "Vias", Toast.LENGTH_LONG).show();
         } else if (id == R.id.nav_send) {
-            Toast.makeText(MainActivity.this, "Enviar", Toast.LENGTH_LONG).show();
+            Toast.makeText(MainActivity.this, "Edifícios", Toast.LENGTH_LONG).show();
         }
-
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         if (drawer != null) {
@@ -138,14 +355,56 @@ public class MainActivity extends AppCompatActivity
         mMap.addMarker(new MarkerOptions().position(Amadora).title("Onde eu estou!"));*/
 
         LatLng Agualva = new LatLng(38.77410061,-9.2924664);
-        GroundOverlayOptions AgualvaMap = new GroundOverlayOptions()
+      /*  GroundOverlayOptions AgualvaMap = new GroundOverlayOptions()
                 .image(BitmapDescriptorFactory.fromResource(R.mipmap.mapserv))
                 .position(Agualva, 12500f, 9200f);
-        mMap.addGroundOverlay(AgualvaMap);
+        mMap.addGroundOverlay(AgualvaMap);*/
 
         float zoomLevel = 13;
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Agualva, zoomLevel));
         mMap.setMyLocationEnabled(true);
             }
 
+
+    private void setUpMapIfNeeded() {
+        if (mMap == null) {
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            } else {
+                Log.e("WMSDEMO", "Map was null!");
+            }
+        }
     }
+
+
+    private void setUpMap() {
+
+        StringBuilder sb = new StringBuilder();
+        Cursor resultSet = this.mydatabase.rawQuery("Select name from layers where active", null);
+        Log.e("LAYERS: ", String.valueOf(resultSet.getCount()));
+        resultSet.moveToFirst();
+        if (resultSet.moveToFirst()) {
+            do {
+                if (sb.length()<1)
+                    sb.append(resultSet.getString(0));
+                else
+                    sb.append("," + resultSet.getString(0));
+            } while (resultSet.moveToNext());
+        }
+        if (!resultSet.isClosed()) {
+            resultSet.close();
+        }
+        Log.e("LAYERS: ", sb.toString());
+
+        TileProvider wmsTileProvider = TileProviderFactory.getOsgeoWmsTileProvider(sb.toString());
+        mMap.addTileOverlay(new TileOverlayOptions().tileProvider(wmsTileProvider));
+
+        // Because the demo WMS layer we are using is just a white background map, switch the base layer
+        // to satellite so we can see the WMS overlay.
+       // mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+    }
+}
+
